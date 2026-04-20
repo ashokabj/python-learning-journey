6900b6c90ac80d06f2bd2895de81ed7f23e03328
ashokabj <ashokbj28@gmail.com>
2026-04-20 22:36:26 +0530

    Add list operations: access, modify, add, remove...

---
+++ list_operations.py
@@ -0,0 +1,30 @@
+# List creation
+numbers = [1, 2, 3, 4]
+
+# Access
+print(numbers[0])
+print(numbers[-1])
+
+# Modify
+numbers[1] = 10
+
+# Add
+numbers.append(5)
+numbers.insert(1, 7)
+
+# Remove
+numbers.remove(3)
+numbers.pop()
+
+# Length
+print(len(numbers))
+
+
+
+# List comprehension 
+squares = [x**2 for x in numbers]
+print(squares)
+
+# Sorting
+numbers.sort()
+print(numbers)
