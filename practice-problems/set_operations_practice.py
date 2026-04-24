a7b622ada0a6e87cbe4bd08bb1da8d4c7efbdb58
ashokabj <ashokbj28@gmail.com>
2026-04-24 13:11:33 +0530

    Add set practice: student comparision and duplicate removal

--- practice-problems/set_operations_practice.py
+++ practice-problems/set_operations_practice.py
@@ -10,3 +10,9 @@ print(f"Common students: {setA & setB}")
 print(f"All students: {setA | setB}")
 print(f"Only in class A: {setA - setB}")
 print(f"Only in class B: {setB - setA}")
+
+#Removing duplicates
+roll_numbers = [10, 20, 28, 10, 37, 28, 77, 77]
+unique_rolls = set(roll_numbers)
+
+print(f"Unique roll numbers: {unique_rolls}")
