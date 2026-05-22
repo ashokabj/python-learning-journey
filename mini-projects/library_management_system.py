61db0d921cd1845fa779531279562f67f36b9e40
ashokabj <ashokbj28@gmail.com>
2026-05-22 23:20:26 +0530

    Build console-based library management system

---
+++ mini-projects/library_management_system.py
@@ -0,0 +1,63 @@
+library = {
+    "Python Basics": "Available",
+    "AI for Beginners": "Available",
+    "Data Science 101": "Available"
+}
+
+borrowed_books = []
+
+while True:
+
+    print("\n===== Library Management System =====")
+    print("1. View Books")
+    print("2. Borrow Book")
+    print("3. Return Book")
+    print("4. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+
+        print("\nAvailable Books:")
+        for book, status in library.items():
+            print(f"{book} --> {status}")
+
+    elif choice == "2":
+
+        book_name = input("Enter book name to borrow: ")
+
+        if book_name in library:
+
+            if library[book_name] == "Available":
+                library[book_name] = "Borrowed"
+                borrowed_books.append(book_name)
+
+                print("Book borrowed successfully ")
+
+            else:
+                print("Book is already borrowed ")
+
+        else:
+            print("Book not found ")
+
+    elif choice == "3":
+
+        return_book = input("Enter book name to return: ")
+
+        if return_book in borrowed_books:
+
+            library[return_book] = "Available"
+            borrowed_books.remove(return_book)
+
+            print("Book returned successfully ")
+
+        else:
+            print("This book was not borrowed ")
+
+    elif choice == "4":
+
+        print("Exiting Library System ")
+        break
+
+    else:
+        print("Invalid Choice ")
