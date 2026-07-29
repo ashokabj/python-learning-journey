03bf83fbbc366e097a463bf76e59406dc759f428
ashokabj <ashokbj28@gmail.com>
2026-07-29 23:25:27 +0530

    Implement encapsulation using an employee class

---
+++ core-concepts/employee_emasculation.py
@@ -0,0 +1,35 @@
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.__salary = salary
+
+    def show_salary(self):
+        return f"{self.name}'s salary is {self.__salary}"
+
+    def increase_salary(self, amount):
+        if amount <= 0:
+            return "Inavalid increment amount"
+        else:
+            self.__salary += amount
+            return "Salary updated successfully."
+
+    def decrease_salary(self, amount):
+        if amount > self.__salary:
+            return "Cannot reduce salary below Rs. 0"
+        elif 0 < amount <= self.__salary:
+            self.__salary -= amount
+            return "Salary reduced successfully."
+        elif amount <= 0:
+            return "Invalid decrement amount."
+
+emp = Employee("Ashok", 50000)
+
+print(emp.show_salary())
+
+print(emp.increase_salary(5000))
+print(emp.show_salary())
+
+print(emp.decrease_salary(10000))
+print(emp.show_salary())
+
+
