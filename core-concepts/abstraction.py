aeed20750121e28188c438d174cd3ded765354bb
ashokabj <ashokbj28@gmail.com>
2026-07-29 15:59:48 +0530

    Implement abstraction using abstract base classes

---
+++ core-concepts/abstraction.py
@@ -0,0 +1,22 @@
+# Abstraction
+
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+    @abstractmethod
+    def start(self):
+        pass
+
+class Car(Vehicle):
+    def start(self):
+        print("Car engine started.")
+
+class Bike(Vehicle):
+    def start(self):
+        print("Bike engine started.")
+
+car = Car()
+bike = Bike()
+
+car.start()
+bike.start()
