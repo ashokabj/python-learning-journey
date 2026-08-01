0c5c70ac3855d7e0e7c5ab6b24bf91327ffca7cc
ashokabj <ashokbj28@gmail.com>
2026-08-01 13:38:55 +0530

    Improve code readability with foormatting and inline comments

--- core-concepts/employee_encapsulation.py
+++ core-concepts/employee_encapsulation.py
@@ -1,7 +1,7 @@
 class Employee:
     def __init__(self, name, salary):
         self.name = name
-        self.__salary = salary
+        self.__salary = salary   # Private attribute
 
     def show_salary(self):
         return f"{self.name}'s salary is {self.__salary}"
@@ -9,6 +9,7 @@ class Employee:
     def increase_salary(self, amount):
         if amount <= 0:
             return "Inavalid increment amount"
+        
         else:
             self.__salary += amount
             return "Salary updated successfully."
@@ -16,9 +17,11 @@ class Employee:
     def decrease_salary(self, amount):
         if amount > self.__salary:
             return "Cannot reduce salary below Rs. 0"
+        
         elif 0 < amount <= self.__salary:
             self.__salary -= amount
             return "Salary reduced successfully."
+        
         elif amount <= 0:
             return "Invalid decrement amount."
 
