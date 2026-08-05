71b20cf6f1bceddbf961d7e6ae0fc3f9b1a49623
ashokabj <ashokbj28@gmail.com>
2026-08-05 23:42:46 +0530

    Add polymorphism examples with methd overriding and duck typing

--- core-concepts/polymorphism.py
+++ core-concepts/polymorphism.py
@@ -1,1 +1,46 @@
-# Polymorpism
+"""
+Polymorphism
+
+Concepts covered:
+1. Polymorphism
+2. Method overriding
+3. Runtime polymorphism
+4. Duck typimg
+"""
+
+class Animal:
+    # Parent class
+
+    def sound(self):
+        print("Animal makes a sound.")
+
+class Dog(Animal):
+    # Child class
+
+    # Method overriding
+    def sound(self):
+        print("Dog barks.")
+
+class Cat(Animal):
+    # Child class
+
+    # Method overriding
+    def sound(self):
+        print("Cat meows.")
+
+class Robot:
+    # Unrelated class
+    
+    # Duck typing
+    def sound(self):
+        print("Robot says: Hello Human!")
+
+# Runtime polymorphism using Duck Typing
+def make_sound(obj):
+    obj.sound()
+
+# Different objects responding differently
+objects = [Dog(), Cat(), Robot()]
+
+for obj in objects:
+    make_sound(obj)
